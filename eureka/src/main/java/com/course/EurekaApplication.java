6f88207f2ef393742ce0e2f7a720d3ca1d28391a
laiweixin <1351347427@qq.com>
2021-05-14 14:38:52 +0800

    1.增加eureka子模块作为注册服务器 2.将maven改造成maven父子项目

--- eureka/src/main/java/com/course/EurekaApplication.java
+++ eureka/src/main/java/com/course/EurekaApplication.java
@@ -1,13 +1,13 @@
-package com.onlinecourse.onlinecourse;
+package com.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OnlinecourseApplication {
+public class EurekaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OnlinecourseApplication.class, args);
+		SpringApplication.run(EurekaApplication.class, args);
 	}
 
 }
