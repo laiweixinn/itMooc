56fc2d03317880fe41bdf056429a4f7d6ffeaa09
laiweixin <1351347427@qq.com>
2021-05-14 14:30:14 +0800

    spring初始项目

---
+++ src/test/java/com/onlinecourse/onlinecourse/OnlinecourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlinecourse.onlinecourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinecourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
