cf4bef48c91a38ecefe818b8d823b602686b6f03
laiweixin <1351347427@qq.com>
2021-05-15 23:15:12 +0800

    1.增加login页面 2.增加路由配置，访问/login时，加载login组件

---
+++ admin/src/router.js
@@ -0,0 +1,25 @@
+import Vue from "vue"
+import Router from "vue-router"
+import Login from "@/views/login.vue"
+Vue.use(Router)
+export default new Router({
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes: [
+        {
+            path: "*",
+            redirect: "/login",
+        },
+        {
+            path:"/login",
+            name:"login",
+            component: Login
+        },
+        {
+            path:"/resign",
+            name:"resign",
+            component: () => import("@/views/Resign.vue")
+        },
+
+    ]
+})
---
+++ admin/src/views/Resign.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    hello!我是鑫鑫!
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Resign.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
