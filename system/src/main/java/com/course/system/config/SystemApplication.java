2f264815dd4f3113a6357c6a651ab67fade40e2d
laiweixin <1351347427@qq.com>
2021-05-15 13:16:50 +0800

    1.Example使用示例 使用Example可以快速完成表单的增删改查，熟练掌握后对代码开发效率将有一个质的飞跃

--- system/src/main/java/com/course/system/config/SystemApplication.java
+++ system/src/main/java/com/course/system/config/SystemApplication.java
@@ -6,6 +6,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
