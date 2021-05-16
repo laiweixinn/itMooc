b92b76a8b49ef5e904e8f5feead7f140d9090b33
laiweixin <1351347427@qq.com>
2021-05-16 16:57:09 +0800

    1.通用的sidebar点击激活样式方法，使用watch监听路由变化

--- admin/src/views/admin.vue
+++ admin/src/views/admin.vue
@@ -480,6 +480,26 @@ export default {
     let _this = this;
     $("body").removeClass("login-layout light-login");
     $("body").attr("class", "no-skin");
+    _this.activeSidebar(_this.$route.name.replace("/", "-") + "-sidebar");
+  },
+    watch: {
+      $route: {
+        handler: function (val, oldVal) {
+          // sidebar激活样式方法二
+          console.log("---->页面跳转：", val, oldVal);
+          let _this = this;
+
+          if (!_this.hasResourceRouter(val.name)) {
+            _this.$router.push("/login");
+            return;
+          }
+
+          _this.$nextTick(function () {  //页面加载完成后执行
+            _this.activeSidebar(_this.$route.name.replace("/", "-") + "-sidebar");
+          })
+        }
+      }
+    },
     // // console.log("admin");
     // // sidebar激活样式方法二
     // _this.activeSidebar(_this.$route.name.replace("/", "-") + "-sidebar");
@@ -491,7 +511,6 @@ export default {
     // if (!_this.hasResourceRouter(_this.$route.name)) {
     //   _this.$router.push("/login");
     // }
-  },
   methods: {
 
 
